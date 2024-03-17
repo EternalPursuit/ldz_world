14a72f657c434244b179b73b9c8c861a285ade5c
EP_LDZ_VM <ldz_free@163.com>
2024-03-17 21:25:57 +0800

    this file is for module named argparse.
    in this file, which is demenstrated args.accumulate(args.integers)

---
+++ parser_test.py
@@ -0,0 +1,9 @@
+import argparse
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('integers',type=int,nargs='+')
+parser.add_argument('--sum',dest='accumulate',action='store_const',const=sum,default=max)
+
+args = parser.parse_args()
+print(args.accumulate(args.integers))
